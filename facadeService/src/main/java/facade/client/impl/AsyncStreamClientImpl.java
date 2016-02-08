79ee8e9d125f6600b971996c1024b7d048f1c1b9
afedorov <coceds@gmail.com>
2016-02-08 18:49:21 +0300

    client fix

--- facadeService/src/main/java/facade/client/impl/AsyncStreamClientImpl.java
+++ facadeService/src/main/java/facade/client/impl/AsyncStreamClientImpl.java
@@ -15,7 +15,6 @@ import org.springframework.stereotype.Service;
 import rx.Observable;
 import rx.apache.http.ObservableHttp;
 import rx.apache.http.ObservableHttpResponse;
-import rx.functions.Func1;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
@@ -38,32 +37,79 @@ public class AsyncStreamClientImpl implements AsyncStreamClient {
             }
             final HttpAsyncRequestProducer requestProducer = HttpAsyncMethods.createPost(baseUrl + endPoint.getUrl(), content,
                     ContentType.APPLICATION_JSON);
+
             ObservableHttp<ObservableHttpResponse> req = ObservableHttp.createRequest(requestProducer, asyncClient);
 
 
             final Observable<ObservableHttpResponse> observable = req
-                    .toObservable();
-
-
-            Func1<ObservableHttpResponse, Observable<? extends T>> func = observableHttpResponse -> {
-                return observableHttpResponse.getContent().map(bb ->
-                        {
-                            try {
-                                String value = new String(bb);
-                                value = value.replace("data:", "");
-                                System.out.println(value);
-                                return mapper.readValue(value, endPoint.getTypeReference());
-                            } catch (IOException e) {
-                                throw new RuntimeException(e);
-                            }
+                    .toObservable().doOnCompleted(() -> {
+                        System.out.println("111111111");
+                    });
+            return Observable.create(subscriber -> {
+                observable.subscribe(observableHttpResponse -> {
+                    System.out.println("3");
+                    observableHttpResponse.getContent().subscribe(bytes -> {
+                        System.out.println("4");
+                        try {
+                            String value = new String(bytes);
+                            value = value.replace("data:", "");
+                            System.out.println(value);
+                            subscriber.onNext((T) mapper.readValue(value, endPoint.getTypeReference()));
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
                         }
-                );
-            };
-            final Observable<T> tObservable = observable.flatMap(func);
-//            return tObservable;
-            return Observable.zip(observable, tObservable, (observableHttpResponse, t) -> {
-                return t;
+                    });
+                }, throwable -> {
+                    subscriber.onError(throwable);
+                }, () -> {
+                    subscriber.onCompleted();
+                });
             });
+//            Observable.create(subscriber -> {
+//                observable.map(observableHttpResponse -> {
+//                    observableHttpResponse.getContent().subscribe(subscriber);
+//                    return null;
+//                });
+//            });
+//            Func1<ObservableHttpResponse, Observable<? extends T>> func = observableHttpResponse -> {
+//                Observable<T> o = Observable.create(subscriber -> {
+//                    observableHttpResponse.getContent().map(bb ->
+//                    {
+//                        try {
+//                            String value = new String(bb);
+//                            value = value.replace("data:", "");
+//                            System.out.println(value);
+//                            subscriber.onNext((T) mapper.readValue(value, endPoint.getTypeReference()));
+//                            subscriber.onCompleted();
+//                            return mapper.readValue(value, endPoint.getTypeReference());
+//                        } catch (IOException e) {
+//                            throw new RuntimeException(e);
+//                        }
+//                    });
+//                });
+//                return o;
+//                Observable<byte[]> content1 = observableHttpResponse.getContent();
+//                content1.doOnCompleted(() -> {
+//                    System.out.println("22222222");
+//                });
+//                return content1.map(bb ->
+//                        {
+//                            try {
+//                                String value = new String(bb);
+//                                value = value.replace("data:", "");
+//                                System.out.println(value);
+//                                return mapper.readValue(value, endPoint.getTypeReference());
+//                            } catch (IOException e) {
+//                                throw new RuntimeException(e);
+//                            }
+//                        }
+//                );
+//            };
+//            final Observable<T> tObservable = observable.flatMap(func);
+//            return tObservable;
+//            return Observable.zip(observable, tObservable, (observableHttpResponse, t) -> {
+//                return t;
+//            });
 
         } catch (UnsupportedEncodingException | JsonProcessingException e) {
             throw new RuntimeException(e);
